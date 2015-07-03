30c0e89d1cee7dd4c5555c79c54ac161f65a5412
Gert Geidel <gert.geidel@infopark.de>
2015-07-03 17:07:17 +0200

    SDK 0.65.0

--- README.md
+++ README.md
@@ -4,34 +4,10 @@ This Gem adds a three-column grid widget to your Scrivito app. You can fill the 
 
 ## Prerequisites
 
-Use Bootstrap in your Rails App. (Tested with Bootstrap 3.2)
+Use Bootstrap in your Rails App.
 
 ## Installation
 
 Add the gem to your Gemfile:
 
     gem 'scrivito_three_column_widget'
-
-Run bundle:
-
-    $ bundle
-
-(Currently you need to download the gem's files from github and install it locally.)
-
-Run
-
-    $ rake scrivito:migrate:install
-    $ rake scrivito:migrate
-
-Switch to your Workspace 'rtc' and follow below Steps for Usage. If you are happy with the outcome run
-
-    $ rake scrivito:migrate:publish
-
-
-## Steps for Usage
-
-- Insert the Widget 'Three Columns' somewhere and put a widget into each column that becomes visible.
-- Edit the Widget Properties to modify the column widths.
-
-
-
--- app/models/three_column_widget.rb
+++ app/models/three_column_widget.rb
@@ -1,28 +1,11 @@
 class ThreeColumnWidget < Widget
   attribute :column_1, :widgetlist
-  attribute :column_1_width, :string
-  attribute :column_2, :widgetlist
-  attribute :column_2_width, :string
-  attribute :column_3, :widgetlist
-  attribute :column_3_width, :string
-
-  attribute :column_1, :widgetlist
   attribute :column_2, :widgetlist
   attribute :column_3, :widgetlist
-  attribute :column_1_width, :string
-  attribute :column_2_width, :string
-  attribute :column_3_width, :string
-
-  def column_1_width
-    self[:column_1_width].presence || '4'
-  end
-
-  def column_2_width
-    self[:column_2_width].presence || '4'
-  end
+  attribute :column_1_width, :string, default: '4'
+  attribute :column_2_width, :string, default: '4'
 
   def column_3_width
-    self[:column_3_width].presence || '4'
+    12 - self.column_1_width.to_i - self.column_2_width.to_i
   end
-
 end
